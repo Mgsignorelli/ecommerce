497ab558f5a990bdc696b62b57ae94044f188f56
Micaela Signorelli <m.signorelli@gmail.com>
2019-04-07 12:22:25 +0000

    attempts to fix customer error in checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -43,15 +43,16 @@ def checkout(request):
                     description = request.user.email,
                     card = payment_form.cleaned_data['stripe_id'],
                 )
+                if customer.paid:
+                    messages.error(request, "You have successfully paid")
+                    request.session['cart'] = {}
+                    return redirect(reverse('products'))
+                else:
+                    messages.error(request, "Unable to take payment")
             except stripe.error.CardError:
                 messages.error(request, "Your card was declined!")
                 
-            if customer.paid:
-                messages.error(request, "You have successfully paid")
-                request.session['cart'] = {}
-                return redirect(reverse('products'))
-            else:
-                messages.error(request, "Unable to take payment")
+            
         else:
             print(payment_form.errors)
             messages.error(request, "We were unable to take a payment with that card!")
